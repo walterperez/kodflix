1473d56aca66c7ac22bfb4b17be62add5ba74920
walterperez <waltermaniaco@gmail.com>
2019-04-01 23:45:56 +0100

    Create a Gallery component to show the covers #9

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
 
-//Covers
-import LordOfTheRigns1Cover from "./img/LordOfTheRigns1Cover.jpg";
-import BatmanCover from "./img/BatmanCover.jpg";
-import SlendermanCover from "./img/SlendermanCover.jpg";
-import SpidermanCover from "./img/SpidermanCover.jpg";
-import StarTreckDiscoveryCover from "./img/StarTreckDiscoveryCover.jpg";
-import StarWarsCover from "./img/StarWarsCover.jpg";
-
-//Components
-import Movie from "./components/Movie";
-
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <div className="flex-container">
-          <Movie name={"Lord Of The Rigns"} cover={LordOfTheRigns1Cover} />
-          <Movie name={"Batman"} cover={BatmanCover} />
-          <Movie name={"Slenderman"} cover={SlendermanCover} />
-        </div>
-        <div className="flex-container">
-          <Movie name={"Spiderman"} cover={SpidermanCover} />
-          <Movie name={"Star Treck Discovery"} cover={StarTreckDiscoveryCover} />
-          <Movie name={"StarWars"} cover={StarWarsCover} />
-        </div>
+
       </div>
     );
   }
---
+++ src/components/Gallery.js
@@ -0,0 +1,27 @@
+import React from "react";
+//Components
+import Movie from "./Movie";
+//Covers
+import LordOfTheRigns1Cover from "./../img/LordOfTheRigns1Cover.jpg";
+import BatmanCover from "./../img/BatmanCover.jpg";
+import SlendermanCover from "./../img/SlendermanCover.jpg";
+import SpidermanCover from "./../img/SpidermanCover.jpg";
+import StarTreckDiscoveryCover from "./../img/StarTreckDiscoveryCover.jpg";
+import StarWarsCover from "./../img/StarWarsCover.jpg";
+
+export default function Gallery(props) {
+  return (
+    <div>
+      <div className="flex-container">
+        <Movie name={"Lord Of The Rigns"} cover={LordOfTheRigns1Cover} />
+        <Movie name={"Batman"} cover={BatmanCover} />
+        <Movie name={"Slenderman"} cover={SlendermanCover} />
+      </div>
+      <div className="flex-container">
+        <Movie name={"Spiderman"} cover={SpidermanCover} />
+        <Movie name={"Star Treck Discovery"} cover={StarTreckDiscoveryCover} />
+        <Movie name={"StarWars"} cover={StarWarsCover} />
+      </div>
+    </div>
+  );
+}
