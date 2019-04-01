bc812554201605dffe88ad1f27098ada3ee84210
walterperez <waltermaniaco@gmail.com>
2019-04-01 23:46:59 +0100

    Create a Gallery component to show the covers #9 FULLY WORKING

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
+import Gallery from "./components/Gallery";
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-
+        <Gallery />
       </div>
     );
   }
