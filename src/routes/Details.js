f81490b295929e2a5800ba138c1dfc1f69adc883
walterperez <waltermaniaco@gmail.com>
2019-04-02 13:11:59 +0100

    Introduce component state #13

--- src/routes/Details.js
+++ src/routes/Details.js
@@ -1,11 +1,40 @@
-import React from "react";
-import {Link} from "react-router-dom";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-export default function Details({ match }) {
-  return (
-    <div>
-      <h1>Hello, this will be the details page for {match.params.idMovie.split("-").join(" ")}</h1>
-      <Link to="/">Home</Link>
-    </div>
-  );
+export default class Details extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      message: "this will be the details page for each Movie & TV show :)"
+    }
+  }
+
+  componentDidMount () {
+    setTimeout(
+      () => {
+        this.setState({
+        message:"Coming soon! :)"
+        })
+      }
+      , 3000);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>
+          Hello, this will be the details page for{" "}
+          {this.props.match.params.idMovie.split("-").join(" ")}
+        </h1>
+        <hr />
+        <h1>
+          The message state of this component is:
+          <strong>
+            {this.state.message}
+          </strong>
+        </h1>
+        <Link to="/">Home</Link>
+      </div>
+    );
+  }
 }
