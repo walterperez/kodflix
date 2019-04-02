440d68357a610c1df3450838fee0a4c70c6336d6
walterperez <waltermaniaco@gmail.com>
2019-04-02 10:53:19 +0100

    Introduce routing / navigation #8

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import Gallery from "./components/Gallery";
+import Details from "./routes/Details";
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <Gallery />
-      </div>
+      <Router>
+        <div className="container">
+          <Route exact path="/" component={Gallery} />
+          <Route exact path="/Details" component={Details} />
+        </div>
+      </Router>
     );
   }
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,12 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 export default function Movie (props) {
     return (
         <div className="flex-element">
+        <Link to="/Details">
         <img src={props.cover} alt={`${props.name} Cover`} />
+        </Link>
         <div className="box">
           <h1>{props.name}</h1>
         </div>
