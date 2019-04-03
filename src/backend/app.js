94e1d363e2a2bfc257ac1822a880ee745819b2e9
walterperez <waltermaniaco@gmail.com>
2019-04-03 11:21:20 +0100

    Return a list of shows from the backend #19

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-// const port = 3000;
+const movies = require("./moviesDB");
 
-app.set("PORT",3000)
+app.set("PORT",3000);
 
-app.get("/", (req,res)=>{
-    res.send("Hello World!");
+app.get("/rest/shows", (req,res)=>{
+    res.send(movies);
 })
 
 app.listen(app.get("PORT"), ()=>{
---
+++ src/backend/moviesDB.js
@@ -0,0 +1,34 @@
+const moviesDB = [
+    {   
+        "id": "Lord-Of-The-Rings",
+        "title":"Lord Of The Rigns",
+        "synopsis":"The Lord of the Rings: The Fellowship of the Ring is a New Zealand-American 2001 epic fantasy adventure film directed by Peter Jackson based on the first volume of J. R. R. Tolkien's The Lord of the Rings.[5][6][7] It is the first instalment in The Lord of the Rings series, and was followed by The Two Towers (2002) and The Return of the King (2003), based on the second and third volumes of The Lord of the Rings."
+    },
+    {   
+        "id": "Batman",
+        "title": "Batman",
+        "synopsis":"The Dark Knight Rises is a 2012 American-British superhero film directed by Christopher Nolan, who co-wrote the screenplay with his brother Jonathan Nolan, and the story with David S. Goyer.[5] Featuring the DC Comics character Batman, the film is the final installment in Nolan's The Dark Knight Trilogy, and the sequel to The Dark Knight (2008). Christian Bale stars as Bruce Wayne / Batman, alongside Michael Caine, Gary Oldman, Anne Hathaway, Tom Hardy, Marion Cotillard, Joseph Gordon-Levitt, and Morgan Freeman. Eight years after the events of The Dark Knight, merciless revolutionary Bane forces an older Bruce Wayne to resume his role as Batman and save Gotham City from nuclear destruction."
+    },
+    {   
+        "id": "Slenderman",
+        "title": "Slenderman",
+        "synopsis":"Slender Man is a 2018 American supernatural horror film directed by Sylvain White and written by David Birke, based on the character of the same name. The film stars Joey King, Julia Goldani Telles, Jaz Sinclair, Annalise Basso, Alex Fitzalan, and Taylor Richardson, with Javier Botet as the title creature."
+    },
+    {   
+        "id": "Spiderman",
+        "title": "Spiderman",
+        "synopsis":"The Amazing Spider-Man is a 2012 American superhero film based on the Marvel Comics character Spider-Man, and sharing the title of the character's longest-running comic book. It is the fourth theatrical Spider-Man film produced by Columbia Pictures and Marvel Entertainment, and a reboot of Sam Raimi's Spider-Man 2002–2007 trilogy preceding it."
+    },
+    {   
+        "id": "Star-Treck-Discovery",
+        "title":"Star Treck Discovery",
+        "synopsis":"Star Trek: Discovery is an American web television series created for CBS All Access by Bryan Fuller and Alex Kurtzman. It is the first series developed specifically for that service, and the first Star Trek series since Star Trek: Enterprise concluded in 2005. Set roughly a decade before the events of the original Star Trek series and separate from the timeline of the concurrently produced feature films, Discovery follows the crew of the USS Discovery on various adventures."
+    },
+    {   
+        "id": "StarWars",
+        "title":"StarWars",
+        "synopsis":"Star Wars is an American epic space opera franchise, created by George Lucas, which began with the eponymous 1977 movie. The saga quickly became a worldwide pop culture phenomenon.  "
+    }
+];
+
+module.exports = moviesDB;
