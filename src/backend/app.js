4ad3c1870e5caa1b20da41b5d47a9fd1e3767d4d
walterperez <waltermaniaco@gmail.com>
2019-04-03 13:15:59 +0100

    Run frontend and backend servers in parallel #21

--- src/backend/app.js
+++ src/backend/app.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 const movies = require("./moviesDB");
 
-app.set("PORT",3000);
+//Settings
+app.set("PORT", 3001);
 
+//Routes
 app.get("/rest/shows", (req,res)=>{
     res.send(movies);
 })
 
+//Server Run
 app.listen(app.get("PORT"), ()=>{
     console.log(`Server running on port: ${app.get("PORT")}`);
 })
