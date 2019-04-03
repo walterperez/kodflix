a77dbc5441fe12dc4a383677be73121597f0a89c
walterperez <waltermaniaco@gmail.com>
2019-04-03 11:03:48 +0100

    Introduce a back-end server #18

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+// const port = 3000;
+
+app.set("PORT",3000)
 
 app.get("/", (req,res)=>{
     res.send("Hello World!");
 })
 
-app.listen(port, ()=>{
-    console.log(`Server running on port: ${port}`);
+app.listen(app.get("PORT"), ()=>{
+    console.log(`Server running on port: ${app.get("PORT")}`);
 })
