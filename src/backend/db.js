d5bdcdba03081ec060822f22ec743f133bad83d1
walterperez <waltermaniaco@gmail.com>
2019-04-12 11:17:56 +0100

    #fixed bug

--- src/backend/db.js
+++ src/backend/db.js
@@ -6,42 +6,28 @@ require("dotenv/config");
 let url = "";
 let dbName = "";
 if (process.env.NODE_ENV) {
-  console.log(process.env.NODE_ENV)
+  console.log(process.env.NODE_ENV);
 
   //If we are in PRODUCTION
   url = process.env.DB_URL_PRD;
-  console.log( url)
+  console.log(url);
   dbName = "Kodflix"; //K in caps in production
 } else {
   // If we are in DEV
-  console.log("we are in dev local")
+  console.log("we are in dev local");
   url = "mongodb://localhost:27017";
   dbName = "kodflix"; //k in lowercase in DEV
 }
 
 function connect() {
-  const client = new MongoClient(url,{ useNewUrlParser: true });
+  const client = new MongoClient(url, { useNewUrlParser: true });
   return new Promise(resolve => {
     client.connect(function(err) {
       assert.equal(null, err);
       const db = client.db(dbName);
       console.log("Connected successfully to server: ", db.s.databaseName);
-
-const url = "mongodb://localhost:27017";
-const dbName  = "kodflix";
-
-function connect() {
-  const client = new MongoClient(url);
-  return new Promise(resolve => {
-    client.connect(function(err) {
-      assert.equal(null, err);
-      console.log("Connected successfully to server");
-      const db = client.db(dbName);
-      
-      resolve(db);
     });
   });
 }
-})
-}
+
 module.exports = { connect };
