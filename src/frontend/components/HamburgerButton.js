632f6a9ffd89c5c7c341bdaa2ac15288e462a6ef
walterperez <waltermaniaco@gmail.com>
2019-05-11 23:02:15 +0100

    Create the vertical menu panel #34

--- src/frontend/components/HamburgerButton.js
+++ src/frontend/components/HamburgerButton.js
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import hamburgerButtonIcon from "./../common/img/hamburgerIcon.svg";
 
-export default function HamburgerButton() {
-  return (
-    <div className="HamburgerButton">
-      <img
-        className="HamburgerButton-icon"
-        src={hamburgerButtonIcon}
-        alt="hamburger button"
-      />
-    </div>
-  );
+export default class HamburgerButton extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isMenuActive: false,
+      isVisible: "visible"
+    };
+  }
+
+  handleClick() {
+    this.setState({
+      isMenuActive: !this.state.isMenuActive
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <div className="HamburgerButton" onClick={() => this.handleClick()}>
+          <img
+            className="HamburgerButton-icon"
+            src={hamburgerButtonIcon}
+            alt="hamburger button"
+          />
+        </div>
+        <div
+          className={this.state.isMenuActive ? "translucid_backgroud" : null}
+        >
+          <div
+            className={`Menu-List ${
+              this.state.isMenuActive ? this.state.isVisible : null
+            }`}
+          >
+            <p>Hello Menu!</p>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
