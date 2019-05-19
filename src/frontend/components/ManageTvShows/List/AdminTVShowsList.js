94dc2d7450534d39c0585d947607b388cb5560d2
walterperez <waltermaniaco@gmail.com>
2019-05-19 01:59:43 +0100

    Build the list of TV shows on Admin #43

--- src/frontend/components/ManageTvShows/List/AdminTVShowsList.js
+++ src/frontend/components/ManageTvShows/List/AdminTVShowsList.js
@@ -1,6 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 import "./AdminTVShowsList.scss";
+import Loader from "./../../Loader/Loader";
+export default class AdminTVShowsList extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      shows: []
+    };
+  }
 
-export default function AdminTVShowsList() {
-  return <div className="AdminTVShowsList">AdminTVShowsList</div>;
+  componentDidMount() {
+    fetch("/rest/shows")
+      .then(response => response.json())
+      .then(shows => {
+        this.setState({ shows });
+      });
+  }
+
+  render() {
+    let { shows } = this.state;
+    if (shows) {
+      return (
+        <div className="AdminTVShowsList">
+          <h3 className="AdminTVShowsList__title">List of avanibles movies:</h3>
+          <ul className="AdminTVShowsList__List__TVShows">
+            {shows.map((show, index) => {
+              return (
+                <li className="AdminTVShowsList__List__item--show" key={index}>
+                  {show.title}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      );
+    } else {
+      return <Loader />;
+    }
+  }
 }
